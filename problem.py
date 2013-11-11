ff0e16a2f1da8b19e3eadbdc5f2c657658ba671f
Albert Wang <albertyw@mit.edu>
2013-11-10 19:00:40 -0500

    Replaced the longest common substring strategy with one that only tries the beginning and end of strings

--- problem.py
+++ problem.py
@@ -20,11 +20,13 @@ def align_strings(strings, largest_string):
         best_location = None
         best_string_index = None
         for i, string in enumerate(strings):
-            substring_length, location, substring = longest_common_substring(largest_string, string)
+            substring_length, location = longest_common_substring(largest_string, string)
             if substring_length > max_length:
                 max_length = substring_length
                 best_location = location
                 best_string_index = i
+        if best_string_index == None:
+            break
         best_string = strings.pop(best_string_index)
 
         # Combine longest common substring
@@ -40,22 +42,25 @@ def align_strings(strings, largest_string):
         middle = best_string
         after = largest_string[cut_after:]
 
-        print largest_string
-        print best_string
-        print substring
-        print 'before',before
-        print 'middle',middle
-        print 'after ',after
+        #print largest_string
+        #print best_string
+        #print 'before',before
+        #print 'middle',middle
+        #print 'after ',after
 
         if largest_string not in before + middle + after:
             qwerqwer
         largest_string = before + middle + after
-        print largest_string
-        print '---'
+        #print largest_string
+        #print '---'
+    if len(strings) > 0:
+        for string in strings:
+            assert string in largest_string
     return largest_string
 
 # Simple dynamic programming longest common substring algorithm
 #
+"""
 def longest_common_substring(string1, string2):
     matrix = [[0]*len(string2) for _ in string1]
     longest_length = 0
@@ -74,7 +79,17 @@ def longest_common_substring(string1, string2):
             else:
                 matrix[i][j] = 0
     return longest_length, location, substring
-
+"""
+# Shift string2 around to find the largest match at the beginning and at the end of string1
+# Assumes that len(string2) <= len(string1)
+#
+def longest_common_substring(string1, string2):
+    for i in reversed(range(len(string2))):
+        if string1[:i] == string2[len(string2)-i:]:
+            return i, i - len(string2)
+        if string1[len(string1)-i:] == string2[:i]:
+            return i, len(string1) - i
+    return 0, 0
 
 if __name__ == "__main__":
     strings = read_data()
