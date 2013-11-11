e561aefa3d26c0c40bac0d9ca691ddd287cc6c44
Albert Wang <albertyw@mit.edu>
2013-11-10 20:25:58 -0500

    Rewrote align_strings to compare every pair of strings

--- problem.py
+++ problem.py
@@ -19,34 +19,41 @@ def read_data():
     strings = data.split(';')
     return strings
 
-# Given a list of strings and a seed string, continuously move strings from the
-# list into the largest string
-def align_strings(strings, largest_string):
-    while len(strings) > 0:
+# Given a list of strings, continuously combine strings
+def align_strings(strings):
+    # Sort from largest to smallest
+    strings = sorted(strings, key=lambda x: -len(x))
+    longest_string = ''
+    while len(strings) > 1:
         # Find longest common substring
-        matches = ['']*len(strings)
-        for i, string in enumerate(strings):
-            substring_length, location = longest_common_headtail(largest_string, string)
-            matches[i] = (substring_length, location, i)
-
-        best_length, best_location, best_string_index = max(matches, key=lambda x: x[0])
+        matches = []
+        for i, string1 in enumerate(strings):
+            for j, string2 in enumerate(strings):
+                if j <= i:
+                    continue
+                substring_length, location = longest_common_headtail(string1, string2)
+                matches.append( (substring_length, location, i, j) )
+        best_length, best_location, string1_index, string2_index = max(matches, key=lambda x: x[0])
         if best_length == 0:
             break
-        best_string = strings.pop(best_string_index)
+        string1 = strings.pop(string1_index)
+        if string1_index < string2_index:
+            string2_index -= 1
+        string2 = strings.pop(string2_index)
 
         # Combine longest common substring
         cut_before = max(best_location, 0)
-        cut_after = min(best_location + len(best_string), len(largest_string))
+        cut_after = min(best_location + len(string2), len(string1))
 
-        before = largest_string[:cut_before]
-        after = largest_string[cut_after:]
+        before = string1[:cut_before]
+        after = string1[cut_after:]
 
-        #assert largest_string in before + middle + after
-        largest_string = before + best_string + after
-    #if len(strings) > 0:
-    #    for string in strings:
-    #        assert string in largest_string
-    return largest_string
+        combined_string = before + string2 + after
+        strings.append(combined_string)
+        if len(combined_string) > len(longest_string):
+            longest_string = combined_string
+
+    return longest_string
 
 # Shift string2 around to find the largest match at the beginning and at the end of string1
 # Assumes that len(string2) <= len(string1)
@@ -61,7 +68,6 @@ def longest_common_headtail(string1, string2):
     return 0, 0
 
 ## TESTS
-
 class TestLongestCommonHeadTail(unittest.TestCase):
     def test_non_overlapping(self):
         string1 = 'asdf'
@@ -105,19 +111,20 @@ class TestLongestCommonHeadTail(unittest.TestCase):
 
 class TestAlignStrings(unittest.TestCase):
     def test_single_string(self):
-        self.assertEqual(align_strings([], 'asdf'), 'asdf')
+        self.assertEqual(align_strings(['asdf']), 'asdf')
     def test_two_strings(self):
-        self.assertEqual(align_strings(['asdf'], 'sdfg'), 'asdfg')
+        self.assertEqual(align_strings(['asdf', 'sdfg']), 'asdfg')
     def test_multiple_strings(self):
-        self.assertEqual(align_strings(['asdf','sdfg'], 'dfgh'), 'asdfgh')
+        self.assertEqual(align_strings(['asdf', 'sdfg', 'dfgh']), 'asdfgh')
     def test_internal_strings(self):
-        self.assertEqual(align_strings(['sd'], 'asdf'), 'asdf')
+        self.assertEqual(align_strings(['sd', 'asdf']), 'asdf')
     def test_short_strings(self):
-        self.assertEqual(align_strings(['as','sd'], 'df'), 'asdf')
+        self.assertEqual(align_strings(['as','sd', 'df']), 'asdf')
     def test_longest_substring(self):
-        self.assertEqual(align_strings(['fd', 'dff'],'asdf'), 'asdffd')
+        self.assertEqual(align_strings(['fd', 'dff', 'asdf']), 'asdffd')
     def test_different_longest_substring(self):
-        self.assertEqual(align_strings(['fffd', 'dfff'],'asdf'), 'asdffffd')
+        self.assertEqual(align_strings(['fffd', 'dfff', 'asdf']), 'asdfffd')
+
 
 
 ## MAIN
@@ -128,8 +135,5 @@ if __name__ == "__main__":
         unittest.main()
     elif strings == []:
         print "\n"
-    # Sort from largest to smallest
-    strings = sorted(strings, key=lambda x: -len(x))
-    largest_string = strings.pop(0)
-    print align_strings(strings, largest_string)
+    print align_strings(strings)
 
